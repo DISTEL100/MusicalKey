dfdb10fc09724d236797ad8a380ba2a6762e2060
Jonathan Schneider <jonathan_schneider@posteo.net>
2023-05-20 18:54:56 +0200

    simplify and move funcs to Interval.hs

--- src/MusicalKey/Interval.hs
+++ src/MusicalKey/Interval.hs
@@ -1,9 +1,10 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module MusicalKey.Interval where
 
-import           Data.Group
-import           Data.Ratio (approxRational, (%))
-import           GHC.Float  (fromRat)
+import Data.Group
+import Data.Ratio (approxRational, (%))
+import GHC.Float (fromRat)
 
 newtype Frequency = Freq Float deriving (Enum, Ord, Eq, Show)
 
@@ -14,6 +15,17 @@ class (Ord a, Group a) => Interval a where
   (%<%) :: Frequency -> a -> Frequency
   (Freq f) %<% a = Freq f %>% invert a
 
+iterateInterval :: Interval a => a -> Int -> a
+iterateInterval interval iterations =
+  let iterated = iterate (mappend interval) mempty !! iterations
+   in if iterations >= 0 then iterated else invert iterated
+
+(%+%) :: (Interval a) => a -> a -> a
+(%+%) a b = a `mappend` b
+
+(%-%) :: (Interval a) => a -> a -> a
+(%-%) a b = a `mappend` invert b
+
 newtype Cent = Cent Float deriving (Enum, Ord, Eq, Show)
 instance Semigroup Cent where
   (Cent a) <> (Cent b) = Cent (a + b)
@@ -23,7 +35,7 @@ instance Group Cent where
   invert (Cent a) = Cent (-a)
 instance Interval Cent where
   Freq a %% Freq b = Cent (1200 * logBase 2 (b / a))
-  (Freq f) %>% Cent a = Freq (f * 2**(a/1200))
+  (Freq f) %>% Cent a = Freq (f * 2 ** (a / 1200))
 
 newtype FreqRatio = FreqRatio Rational deriving (Enum, Ord, Eq, Show)
 instance Semigroup FreqRatio where
--- src/MusicalKey/Tuning.hs
+++ src/MusicalKey/Tuning.hs
@@ -2,30 +2,31 @@ module Tuning where
 
 import           Data.Group          (invert)
 import qualified Data.Set            as Set
-import           MusicalKey.Interval (Frequency, Interval, (%>%))
+import           MusicalKey.Interval (Frequency, Interval, (%>%), (%+%), iterateInterval)
 
 type Degree= Int
 
 data TuningSystem a = TuningSystem { intervals::(Interval a)=> Set.Set a, equivalentInterval :: (Interval a) => a }
 
-getInterval :: (Interval a) => TuningSystem a -> Degree -> a
-(getInterval) (TuningSystem set equi) deg =
+class DegreeIndexable a b where 
+  (!!!) :: a -> Degree -> b
+
+instance (Interval b) => DegreeIndexable (TuningSystem b) b where
+ (!!!) (TuningSystem set equi) deg =
   let size = Set.size set :: Int
       index = deg `mod` size :: Int
       eqs = deg `div` size :: Int
       interval = Set.elemAt index set
-      eqInterval = intervalPower equi eqs
-    in if deg >= 0 then eqInterval else invert eqInterval
-
-intervalPower :: (Interval a) => a -> Int -> a
-intervalPower interval power = let iterated = (iterate (mappend interval) mempty) !! power
-                               in if power >= 0 then iterated else invert iterated
+      eqInterval = iterateInterval equi eqs
+    in if deg >= 0 then eqInterval %+% interval else invert eqInterval
 
 data Tuning a = Tuning { system::TuningSystem a, referenceFreq:: Frequency, referenceDegree:: (Degree, Int) }
 
+-- TODO make instance of
+instance (Interval a) => DegreeIndexable (Tuning a) Frequency where
 freqOfDegree :: (Interval a) => Tuning a -> (Degree, Int) -> Frequency
 freqOfDegree (Tuning ts@(TuningSystem intSet eqInt) refFreq (refDeg, refEq)) (deg, eq) =
   let diffEqs = refEq - eq
-      eqInterval = intervalPower eqInt diffEqs
-      degInterval = (invert  $ getInterval ts refDeg) `mappend` (getInterval ts deg)
+      eqInterval = iterateInterval eqInt diffEqs
+      degInterval = (invert  $ ts !!! refDeg) `mappend` (ts !!! deg)
     in refFreq %>% (mappend eqInterval degInterval)

549a0750e81cde81f4bd077d6e7f6f3ee9400c4a
Jonathan Schneider <jonathan_schneider@posteo.net>
2023-05-25 18:46:44 +0200

    new interval definition

--- src/MusicalKey/Interval.hs
+++ src/MusicalKey/Interval.hs
@@ -2,45 +2,23 @@
 
 module MusicalKey.Interval (module MusicalKey.Interval) where
 
-import Data.Group (Group (invert))
-import Data.Ratio (approxRational, (%))
-import GHC.Float (fromRat)
+import Data.Group (Abelian, Group (invert))
+import GHC.Real ( Ratio((:%)), (%) )
 
 newtype Frequency = Freq Float deriving (Enum, Ord, Eq, Show)
 
-class (Ord a, Group a) => Interval a where
-  (%%) :: Frequency -> Frequency -> a
-  (%>%) :: Frequency -> a -> Frequency
-  (Freq f) %>% a = Freq f %<% invert a
-  (%<%) :: Frequency -> a -> Frequency
-  (Freq f) %<% a = Freq f %>% invert a
+data Interval' = Cent Float | Ratio Rational
 
-newtype Cent = Cent Float deriving (Enum, Ord, Eq, Show)
+instance Semigroup Interval' where
+  (Cent a) <> Cent b = Cent $ a + b
+  (Ratio a) <> Ratio b = Ratio $ a * b
+  _ <> _ = error "cant combine ratios with cents without rounding"
 
-instance Semigroup Cent where
-  (Cent a) <> (Cent b) = Cent (a + b)
+instance Monoid Interval' where
+  mempty = Ratio (1 % 1)
 
-instance Monoid Cent where
-  mempty = Cent 0.0
+instance Group Interval' where
+  invert (Cent a) = Cent $ -a
+  invert (Ratio (a :% b)) = Ratio $ b :% a
 
-instance Group Cent where
-  invert (Cent a) = Cent (-a)
-
-instance Interval Cent where
-  Freq a %% Freq b = Cent (1200 * logBase 2 (b / a))
-  (Freq f) %>% Cent a = Freq (f * 2 ** (a / 1200))
-
-newtype FreqRatio = FreqRatio Rational deriving (Enum, Ord, Eq, Show)
-
-instance Semigroup FreqRatio where
-  FreqRatio a <> FreqRatio b = FreqRatio (a + b)
-
-instance Monoid FreqRatio where
-  mempty = FreqRatio (1 % 1)
-
-instance Group FreqRatio where
-  invert (FreqRatio a) = FreqRatio (recip a)
-
-instance Interval FreqRatio where
-  Freq a %% Freq b = FreqRatio (approxRational (a / b) 0.0000001)
-  (Freq f) %>% FreqRatio a = Freq (f * fromRat a)
+instance Abelian Interval'

6e359ed98b8fbaf3061f77d64b78a5c696fe7471
Jonathan Schneider <mail@jonathanschneider.live>
2023-09-12 16:24:55 +0200

    mode

--- src/MusicalKey/Tuning/Mode.hs
+++ src/MusicalKey/Tuning/Mode.hs
@@ -1,15 +1,25 @@
 module MusicalKey.Tuning.Mode (module MusicalKey.Tuning.Mode) where
 
 import MusicalKey.Pitch
-import MusicalKey.Tuning.Tuning (Tuning)
+import MusicalKey.Tuning.Tuning (Tuning, (!>!))
 
-data Mode out = Mode {root :: Degree, scale :: [Degree], tuning :: forall t . (Tuning t out) => t } 
+data Mode out = forall t. (Tuning t out) => Mode Degree [Degree] t
 
-scalePitch :: (IsPitch p) => Mode out -> Pitch p -> Pitch p
-scalePitch (Mode {root = r, scale = s}) p =
+
+root :: Mode out -> Degree
+root (Mode r _ _) = r
+
+scale :: Mode out -> [Degree]
+scale (Mode _ s _) = s
+
+scalePitch :: (IsPitch p) => Degree -> [Degree] -> Pitch p -> Pitch p
+scalePitch r s p =
   let rooted = r + degree p :: Int
       size = length s
       dm = divMod (degree p) size
       newRep = equave p + fst dm
       newDeg = rooted + degree p + s !! snd dm
    in toPitch newDeg newRep
+
+instance Tuning (Mode out) out where
+  Mode r s t !>! p = t !>! scalePitch r s p

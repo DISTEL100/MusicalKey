4b46f602cd853e34a314f0e99c4e4aaadb230d86
Jonathan Schneider <mail@jonathanschneider.live>
2023-09-12 09:21:11 +0200

    remove pitch from TuningSystem

--- src/MusicalKey/TuningSystem.hs
+++ src/MusicalKey/TuningSystem.hs
@@ -6,10 +6,10 @@ import Data.Set qualified as Set
 import MusicalKey.Interval ( Interval )
 import MusicalKey.Pitch (IsPitch (deg, rep, toPitch), Pitch (Pitch))
 
-class TuningSystem a b where
-  (!%) :: a -> Pitch b -> Interval
+class TuningSystem a where
+  (!%) :: forall b. (IsPitch b) => a -> Pitch b -> Interval
 
-instance IsPitch b => TuningSystem (Set.Set Interval) b where
+instance TuningSystem (Set.Set Interval) where
   set !% pitch
     | Set.null set = mempty
     | deg pitch <= 0 =
@@ -17,16 +17,16 @@ instance IsPitch b => TuningSystem (Set.Set Interval) b where
          in Set.elemAt (snd dm) set <> pow (Set.findMax set) (fst dm + rep pitch)
     | otherwise = invert $ set !% invert pitch
 
-instance IsPitch b => TuningSystem [Interval] b where
+instance TuningSystem [Interval] where
   (!%) = (!%) . Set.fromList
 
-instance (Ord b) => TuningSystem (M.Map b Interval) b where
-  freqMap !% (Pitch b) = freqMap M.! b
+instance (IsPitch b) => TuningSystem (M.Map (Pitch b) Interval)  where
+  freqMap !% p = freqMap M.! toPitch (deg p) (rep p)
 
-degreesBetween :: (IsPitch b, TuningSystem a b) => a -> Pitch b -> Pitch b -> Int
+degreesBetween :: forall a b . (IsPitch b, TuningSystem a ) => a -> Pitch b -> Pitch b -> Int
 degreesBetween ts p1 p2 = degreesBetween' ts p1 p2 0
 
-degreesBetween' :: (IsPitch b, TuningSystem a b) => a -> Pitch b -> Pitch b -> Int -> Int
+degreesBetween' :: forall a b . (IsPitch b, TuningSystem a) => a -> Pitch b -> Pitch b -> Int -> Int
 degreesBetween' ts p1 p2 acc
   | ts !% p1 == ts !% p2 = acc
   | ts !% p1 < ts !% p2 = degreesBetween' ts (toPitch (deg p1 + 1) (rep p1)) p2 (acc + 1)

a9f9ce857a8ccd7726916a513ada894fd2a932ea
Jonathan Schneider <mail@jonathanschneider.live>
2023-10-07 15:48:20 +0200

    better mode types

--- src/MusicalKey/Tuning/Mode.hs
+++ src/MusicalKey/Tuning/Mode.hs
@@ -3,24 +3,21 @@ module MusicalKey.Tuning.Mode (module MusicalKey.Tuning.Mode) where
 import MusicalKey.Pitch
 import MusicalKey.Tuning.Tuning (Tuning, (!>!))
 
-data Mode out = forall t. (Tuning t out) => Mode Degree [Degree] (t out)
+newtype Root = Root Degree deriving Show
+newtype Scale = Scale [Degree] deriving Show
 
-root :: Mode out -> Degree
-root (Mode r _ _) = r
-
-scale :: Mode out -> [Degree]
-scale (Mode _ s _) = s
+data Mode out = forall t. (Tuning t out) => Mode {root::Root, scale:: Scale, tun :: t out }
 
 scalePitch :: (IsPitch p) => Degree -> [Degree] -> Pitch p -> Pitch p
 scalePitch r s p =
   let rooted = r + degree p :: Int
       size = length s
-      dm = divMod rooted size 
+      dm = divMod rooted size
       newRep = equave p + fst dm
       newDeg = s !! snd dm
    in toPitch newDeg newRep
 
 instance Tuning Mode out where
-  Mode r s t !>! p = t !>! scalePitch r s p
+  Mode (Root r)  (Scale s) t !>! p = t !>! scalePitch r s p
 
 
